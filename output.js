cece424fb1fb278a5235dfcc7586499102da6123
Joey O. Chua <joeyochua@gmail.com>
2025-01-20 15:25:54 +0800

    add output documentation

--- output.js
+++ output.js
@@ -1,3 +1,8 @@
+/**
+ * Utility script for outputting values to the DOM.
+ * @param string targetId The ID of the element to output to.
+ * @returns [Function, Function] A function that outputs a value to the target, and a function that clears the target.
+ */
 export default function output(targetId) {
   const target = document.getElementById(targetId);
   return [
